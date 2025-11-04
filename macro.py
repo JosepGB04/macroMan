cbf737472c6dcaac1825becced53921b15fd914f
Koma <josepgb04@gmail.com>
2025-11-04 17:39:22 +0100

    JUST IN CASE

---
+++ macro.py
@@ -0,0 +1,29 @@
+import pyautogui, time
+
+time.sleep(1)
+pyautogui.moveTo(54, 552)
+pyautogui.click()
+time.sleep(0.5)
+while(True):
+    pyautogui.moveTo(515, 374)
+    pyautogui.click()
+    time.sleep(0.5)
+
+    pyautogui.moveTo(1482, 956)
+    pyautogui.click()
+    time.sleep(0.5)
+
+    pyautogui.moveTo(185, 482)
+    pyautogui.click()
+    time.sleep(0.5)
+
+    pyautogui.moveTo(926, 916) # craft all 
+    pyautogui.click()
+    time.sleep(120) #first craft 
+
+    
+
+
+
+
+#pyautogui.click() 
